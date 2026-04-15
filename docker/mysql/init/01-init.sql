a4cdf2e1611c0cc71f4752fafcd49333d5d0be45
eduardo de souza <souzabr85@gmail.com>
2026-04-15 19:23:07 -0300

    feat: adiciona tabelas de Prescricao e Estoque

--- docker/mysql/init/01-init.sql
+++ docker/mysql/init/01-init.sql
@@ -1,19 +1,59 @@
--- Arquivo para dados iniciais.
--- Adicione inserts de exemplo para nossos testes e correção.
--- Exemplo simples de inserção de dados.
--- Mostraremos 2 tabelas com 3 colunas cada e relacionadas entre si.
-USE medalerta;
-CREATE TABLE IF NOT EXISTS users (
-		id INT PRIMARY KEY AUTO_INCREMENT,
-		name VARCHAR(255),
-		email VARCHAR(255)
+
+CREATE TABLE Prescricao (
+    idPrescricao INT AUTO_INCREMENT NOT NULL,
+    idUsuario INT NOT NULL,
+    idMedicamento INT NOT NULL,
+    dosagem VARCHAR(50) NOT NULL,
+    frequenciaUso VARCHAR(50),
+    PRIMARY KEY (idPrescricao),
+    FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario),
+    FOREIGN KEY (idMedicamento) REFERENCES Medicamento(idMedicamento)
 );
-CREATE TABLE IF NOT EXISTS posts (
-		id INT PRIMARY KEY AUTO_INCREMENT,
-		title VARCHAR(255),
-		content TEXT,
-		user_id INT,
-		FOREIGN KEY (user_id) REFERENCES users(id)
+
+CREATE TABLE EstoqueMedicamento (
+    idEstoque INT AUTO_INCREMENT NOT NULL,
+    idPrescricao INT NOT NULL,
+    quantidadeTotal INT NOT NULL,
+    quantidadeAtual INT NOT NULL,
+    PRIMARY KEY (idEstoque),
+    FOREIGN KEY (idPrescricao) REFERENCES Prescricao(idPrescricao)
 );
-INSERT IGNORE INTO users (name, email) VALUES ('Alice', 'alice@example.com');
-INSERT IGNORE INTO posts (title, content, user_id) VALUES ('Primeira Postagem', 'Conteúdo da primeira postagem.', 1);
+
+
+INSERT INTO Prescricao (idUsuario, idMedicamento, dosagem, frequenciaUso) 
+VALUES 
+(1, 1, '1 comprimido', 'A cada 8 horas'),
+(1, 4, '1 comprimido', '1 vez ao dia'),
+(2, 2, '40 gotas', 'A cada 6 horas'),
+(3, 3, '1 cápsula', 'A cada 12 horas'),
+(4, 5, '1 comprimido', 'Após o almoço'),
+(4, 7, '1 cápsula', 'Em jejum'),
+(5, 6, '2 gotas', 'Antes de dormir'),
+(6, 8, '1 comprimido', '1 vez ao dia'),
+(7, 9, '1 cápsula', 'Se houver dor'),
+(8, 10, '15 ml', 'A cada 8 horas'),
+(9, 4, '1 comprimido', '1 vez ao dia'),
+(10, 1, '1 comprimido', 'A cada 8 horas'),
+(2, 5, '1 comprimido', 'Após o almoço'),
+(3, 7, '1 cápsula', 'Em jejum'),
+(5, 8, '1 comprimido', '1 vez ao dia');
+
+
+
+INSERT INTO EstoqueMedicamento (idPrescricao, quantidadeTotal, quantidadeAtual) 
+VALUES 
+(1, 30, 29),  
+(2, 30, 29),  
+(3, 600, 560), 
+(4, 14, 14),   
+(5, 30, 29),   
+(6, 30, 29),   
+(7, 300, 300), 
+(8, 10, 10),   
+(9, 20, 19),   
+(10, 150, 135),
+(11, 30, 30),  
+(12, 30, 29),  
+(13, 30, 29),  
+(14, 14, 13),  
+(15, 10, 9);   
